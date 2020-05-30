94e55193cbe2e624caac7adae2e753a9838f7c49
kmchan <kmchan@tencent.com>
2020-05-30 15:27:07 +0800

    Adding Physics Geometry demo

---
+++ IMDT/Assets/IMDT/COURSE/Scripts/PhysGeomRoot.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysGeomRoot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Physics.gravity = new Vector3(0, -2.0f, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
