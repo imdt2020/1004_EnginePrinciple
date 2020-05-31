33fea1e5237c03f5c37c13a88d702eff3336b31e
kmchan <kmchan@tencent.com>
2020-05-31 18:23:32 +0800

    Adding ragdoll demo

--- IMDT/Assets/IMDT/COURSE/Scripts/VehicleDemoRoot.cs
+++ IMDT/Assets/IMDT/COURSE/Scripts/VehicleDemoRoot.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class VehicleDemoRoot : MonoBehaviour
 {
-    public GameObject[] treePrefab;
-
+    public GameObject[] propsPrefab;
+    public int propsCount = 128;
+    public float mapSize = 300;
+    
     Vector3 Next(int n)
     {
         float g = 1.32471795724474602596f;
@@ -13,17 +15,17 @@ public class VehicleDemoRoot : MonoBehaviour
         float a2 = 1.0f/(g*g);
         float x = Mathf.PingPong(0.5f+a1*n, 1.0f) - 0.5f;
         float z = Mathf.PingPong(0.5f+a2*n, 1.0f) - 0.5f;
-        return new Vector3(x * 100, -1, z * 100);
+        return new Vector3(x * mapSize, -1, z * mapSize);
     }
 
     // Start is called before the first frame update
     void Start()
     {
         Vector3 startPoint = new Vector3(0, 0, 0);
-        if (treePrefab != null && treePrefab.Length > 0)
+        if (propsPrefab != null && propsPrefab.Length > 0)
         {
             int n = 0;
-            for(int i = 0; i < 64; ++i)
+            for(int i = 0; i < propsCount; ++i)
             {
                 Vector3 p = Next(n++);
 
@@ -31,8 +33,8 @@ public class VehicleDemoRoot : MonoBehaviour
                 {
                     p = Next(n++);
                 }
-                int t = i % treePrefab.Length;
-                Instantiate(treePrefab[t], p, Quaternion.identity);
+                int t = i % propsPrefab.Length;
+                Instantiate(propsPrefab[t], p, Quaternion.identity);
             }
         }
     }
